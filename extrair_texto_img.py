fe21a8512f2442cb36380c6e72f6dfd213f26b6e
diogo <d.m.nagasse@gmail.com>
2024-08-06 10:24:33 -0300

    remoção de comentarios desnecessarios

--- extrair_texto_img.py
+++ extrair_texto_img.py
@@ -1,25 +1,23 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-# Defina o caminho para o executável do Tesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# Configuração personalizada para o Tesseract
 config_personalizada = r'--oem 3 --psm 6'
 
 try:
-    # Tente abrir a imagem
-    imagem = Image.open(r'C:\Users\diogo_nagasse\Desktop\PROJETO_OCR\ocr_project\imagens_pdfs\kindle.jpg')  # Altere para o caminho correto da imagem
+    imagem = Image.open(r'C:\Users\diogo_nagasse\Desktop\PROJETO_OCR\ocr_project\imagens_pdfs\kindle.jpg')
 except IOError:
     print("Erro ao abrir a imagem. Verifique o caminho e o formato do arquivo.")
     exit()
 
 # Aplique melhorias na imagem
-imagem = imagem.convert('L')  # Converte para escala de cinza
-imagem = ImageEnhance.Contrast(imagem).enhance(2)  # Aumenta o contraste
-imagem = imagem.filter(ImageFilter.SHARPEN)  # Aplica nitidez
+imagem = imagem.convert('L')
+imagem = ImageEnhance.Contrast(imagem).enhance(3)
+imagem = imagem.filter(ImageFilter.SHARPEN)  
+imagem = imagem.filter(ImageFilter.MedianFilter()) 
 
-# Extraia o texto especificando o idioma
+
 texto = pytesseract.image_to_string(imagem, lang='por', config=config_personalizada)
 print("Texto extraído:")
 print(texto)
--- extrair_txt_pdf.py
+++ extrair_txt_pdf.py
@@ -13,18 +13,14 @@ def txt_pdf(file_path):
 
 caminho_arquivo= r'C:\Users\diogo_nagasse\Desktop\PROJETO_OCR\ocr_project\imagens_pdfs\pdf.pdf'
 texto_extraido = txt_pdf(caminho_arquivo)
-
-# Caminho para a pasta "Downloads" do usuário
 pasta_download = os.path.join(os.path.expanduser('~'), 'Downloads')
 
-# Nome do arquivo de saída
+
 arquivo_de_saida = os.path.join(pasta_download, 'texto_extraido.docx')
 
-#
+
 doc = Document()
 doc.add_paragraph(texto_extraido)
-
-# Salva o documento
 doc.save(arquivo_de_saida)
 
 print(f'Texto extraído salvo em: {arquivo_de_saida}')
